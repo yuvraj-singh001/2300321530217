abb90f1dde99bb9892472b5be6cd7e82878f5c4e
Yuvraj Singh <yuvrajkypn@gmail.com>
2026-06-09 11:49:37 +0530

    Remove request logger middleware function
    Removed createRequestLogger function and its exports.

--- logging_middleware/src/requestLogger.js
+++ logging_middleware/src/requestLogger.js
@@ -9,38 +9,4 @@ function getRequestId(req) {
   );
 }
 
-function createRequestLogger(options = {}) {
-  const logger = options.logger || createLogger(options);
-  const includeHeaders = options.includeHeaders === true;
 
-  return function requestLogger(req, res, next) {
-    const startedAt = process.hrtime.bigint();
-    const requestId = getRequestId(req);
-
-    req.requestId = requestId;
-    res.setHeader?.("x-request-id", requestId);
-
-    res.on("finish", () => {
-      const durationMs = Number(process.hrtime.bigint() - startedAt) / 1_000_000;
-      const statusCode = res.statusCode;
-      const level = statusCode >= 500 ? "error" : statusCode >= 400 ? "warn" : "info";
-
-      logger.log(level, "HTTP request completed", {
-        requestId,
-        method: req.method,
-        path: req.originalUrl || req.url,
-        statusCode,
-        durationMs: Math.round(durationMs * 100) / 100,
-        ip: req.ip || req.socket?.remoteAddress,
-        userAgent: req.headers?.["user-agent"],
-        ...(includeHeaders ? { headers: req.headers } : {})
-      });
-    });
-
-    next();
-  };
-}
-
-module.exports = {
-  createRequestLogger
-};
